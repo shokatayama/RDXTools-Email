b98e22afa1c7bb27408f32d379b1437371eba466
fhayashi843 <34791266+fhayashi843@users.noreply.github.com>
2018-01-03 11:53:54 +0900

    Update TestMain.java

--- 検証用サンプルプロジェクト/src/main/java/jp/co/rdx/tools/email/TestMain.java
+++ 検証用サンプルプロジェクト/src/main/java/jp/co/rdx/tools/email/TestMain.java
@@ -8,6 +8,10 @@ import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.SimpleEmail;
+import java.io.StringWriter;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 
 /**
  * 検証用メインクラス。<br>
@@ -58,6 +62,9 @@ public class TestMain {
 
     // [テスト]メール関連の設定が送信に必要なためコメントアウト。
     // sendMail();
+    
+    // テンプレートに値を埋め込む
+    velocityTest();
   }
 
   /**
@@ -98,6 +105,70 @@ public class TestMain {
       email.send();
     } catch (Exception e) {
       e.printStackTrace();
+    }
+  }
+  
+    private void velocityTest() {
+    // お客さまの情報を作成
+    Visitor visitor = new Visitor("近藤", "おつかれさんでした");
+
+    // ベロシティを初期化
+    Velocity.init();
+    // コンテキストのインスタンスを取得
+    VelocityContext context = new VelocityContext();
+
+    // コンテキストに値を入力
+    context.put("visitor", visitor);
+
+    StringWriter sw = new StringWriter();
+    // テンプレートの作成
+    Template template = Velocity.getTemplate("test.vm", "UTF-8");
+    // テンプレートとマージ
+    template.merge(context, sw);
+    // マージしたデータはWriterオブジェクトであるswが持っているのでそれを文字列として出力
+    System.out.println(sw.toString());
+    sw.flush();
+  }
+
+  /**
+  ユーザ情報を管理するクラス.
+  */
+  public class Visitor {
+    /**
+    * お客さまの名前
+    */
+    private String name = "";
+    /**
+    * 最後の一言
+    */
+    private String end = "";
+
+    /**
+    * 訪問者
+    */
+    public Visitor() {}
+
+    /**
+    * @param name お客様の名前
+    * @param end 最後の一言
+    */
+    public Visitor(String name, String end) {
+      this.name = name;
+      this.end = end;
+    }
+
+    /**
+     * @return 名前取得
+     */
+    public String getName() {
+      return name;
+    }
+
+    /**
+     * @return 最後の一言取得
+     */
+    public String getEnd() {
+      return end;
     }
   }
 }
